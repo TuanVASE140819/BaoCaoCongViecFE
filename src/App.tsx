d3435f73ce2dbcf552e76b391124511db3cc11b7
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2024-10-15 10:37:59 +0700

    change src

--- src/App.tsx
+++ src/App.tsx
@@ -1,191 +1,44 @@
-import { useEffect, useState } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import 'src/global.css';
 
-import Loader from './common/Loader';
-import PageTitle from './components/PageTitle';
-import SignIn from './pages/Authentication/SignIn';
-import SignUp from './pages/Authentication/SignUp';
-import Calendar from './pages/Calendar';
-import Chart from './pages/Chart';
-import ECommerce from './pages/Dashboard/ECommerce';
-import FormElements from './pages/Form/FormElements';
-import FormLayout from './pages/Form/FormLayout';
-import Profile from './pages/Profile';
-import Settings from './pages/Settings';
-import Tables from './pages/Tables';
-import Alerts from './pages/UiElements/Alerts';
-import Buttons from './pages/UiElements/Buttons';
-import DefaultLayout from './layout/DefaultLayout';
-import WorkReport from './components/WorkReport'; // Thêm import cho WorkReport
-import EmployeeList from './components/EmployeeList';
-import EmployeeReports from './components/EmployeeReports';
-import RoleList from './components/RoleList';
+import Fab from '@mui/material/Fab';
 
-function App() {
-  const [loading, setLoading] = useState<boolean>(true);
-  const { pathname } = useLocation();
+import { Router } from 'src/routes/sections';
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
 
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
+import { ThemeProvider } from 'src/theme/theme-provider';
 
-  return loading ? (
-    <Loader />
-  ) : (
-    <DefaultLayout>
-      <Routes>
-        <Route
-          index
-          element={
-            <>
-              <PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <ECommerce />
-            </>
-          }
-        />
-        <Route
-          path="/calendar"
-          element={
-            <>
-              <PageTitle title="Calendar | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Calendar />
-            </>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <>
-              <PageTitle title="Profile | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Profile />
-            </>
-          }
-        />
-        <Route
-          path="/forms/form-elements"
-          element={
-            <>
-              <PageTitle title="Form Elements | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <FormElements />
-            </>
-          }
-        />
-        <Route
-          path="/forms/form-layout"
-          element={
-            <>
-              <PageTitle title="Form Layout | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <FormLayout />
-            </>
-          }
-        />
-        <Route
-          path="/tables"
-          element={
-            <>
-              <PageTitle title="Tables | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Tables />
-            </>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <>
-              <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Settings />
-            </>
-          }
-        />
-        <Route
-          path="/chart"
-          element={
-            <>
-              <PageTitle title="Basic Chart | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Chart />
-            </>
-          }
-        />
-        <Route
-          path="/ui/alerts"
-          element={
-            <>
-              <PageTitle title="Alerts | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Alerts />
-            </>
-          }
-        />
-        <Route
-          path="/ui/buttons"
-          element={
-            <>
-              <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Buttons />
-            </>
-          }
-        />
-        <Route
-          path="/auth/signin"
-          element={
-            <>
-              <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <SignIn />
-            </>
-          }
-        />
-        <Route
-          path="/auth/signup"
-          element={
-            <>
-              <PageTitle title="Signup | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <SignUp />
-            </>
-          }
-        />
-        <Route
-          path="/work-report" // Thêm route cho WorkReport
-          element={
-            <>
-              <PageTitle title="Work Report | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <WorkReport />
-            </>
-          }
-        />
-        <Route
-          path="/employee-list" // Thêm route cho EmployeeList
-          element={
-            <>
-              <PageTitle title="Employee List | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <EmployeeList />
-            </>
-          }
-        />
-        <Route
-          path="/employee-reports" // Thêm route cho EmployeeReports
-          element={
-            <>
-              <PageTitle title="Employee Reports | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <EmployeeReports />
-            </>
-          }
-        />
-        {/* RoleList */}
-        <Route
-          path="/role-list" // Thêm route cho RoleList
-          element={
-            <>
-              <PageTitle title="Role List | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <RoleList />
-            </>
-          }
-        />
-      </Routes>
-    </DefaultLayout>
+import { Iconify } from 'src/components/iconify';
+
+// ----------------------------------------------------------------------
+
+export default function App() {
+  useScrollToTop();
+
+  const githubButton = (
+    <Fab
+      size="medium"
+      aria-label="Github"
+      href="https://github.com/minimal-ui-kit/material-kit-react"
+      sx={{
+        zIndex: 9,
+        right: 20,
+        bottom: 20,
+        width: 44,
+        height: 44,
+        position: 'fixed',
+        bgcolor: 'grey.800',
+        color: 'common.white',
+      }}
+    >
+      <Iconify width={24} icon="eva:github-fill" />
+    </Fab>
+  );
+
+  return (
+    <ThemeProvider>
+      <Router />
+      {githubButton}
+    </ThemeProvider>
   );
 }
-
-export default App;
